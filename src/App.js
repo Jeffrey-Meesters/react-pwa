0571de82cc93747dcbdcc6c857100ea2aea139d9
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2018-12-19 22:02:20 +0100

    I have styled 4 buttons and added 4 functions with 1 button component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,43 @@ import Button from './components/Button';
 
 class App extends Component {
   
-  clickHandler = () => {
-    console.log('hi');
+  clickHiHandler = () => {
+    console.log('hi there');
+  }
+
+  clickByeHandler = () => {
+    console.log('Bye Bye');
+  }
+
+  clickYoloHandler = () => {
+    console.log('Yolo');
+  }
+
+  clickWTFHandler = () => {
+    console.log('WTF!');
   }
   
   render() {
     return (
       <div className="App">
         <h1>Hi I'm still here!</h1>
-        <Button btnText="Click me!" btnClass="thisPage primaryBtn" btnFunction={this.clickHandler} />
+        <div className="button-wapper">
+          <div className="spacer hi-btn">
+            <Button btnText="hi there!" btnClass="btn primary" btnFunction={this.clickHiHandler} />
+          </div>
+          
+          <div className="spacer">
+            <Button btnText="Bye bye" btnClass="btn secondary" btnFunction={this.clickByeHandler} />
+          </div>
+          
+          <div className="spacer">
+            <Button btnText="Yolo!" btnClass="btn light" btnFunction={this.clickYoloHandler} />
+          </div>
+          
+          <div className="spacer">
+            <Button btnText="Click me!" btnClass="btn dark" btnFunction={this.clickWTFHandler} />
+          </div>
+        </div>
       </div>
     );
   }
